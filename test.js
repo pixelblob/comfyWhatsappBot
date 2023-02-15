209c4401f1a6f0011a7d2baa5657aae9455d99e4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 22:25:47 +0200

    random ass file

--- test.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios")
-const cheerio = require("cheerio")
-
-axios.get("https://www.riddles.nu/random").then(req=>{
-    const $ = cheerio.load(req.data)
-    console.log($("blockquote > p").first().text())
-    console.log($("blockquote > div > div").first().text())
-})
